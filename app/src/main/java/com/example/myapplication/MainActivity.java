773cdbcdeadd70162e56229611d3101fb25de664
Luna-4314 <143222153+LunaMeow-0115@users.noreply.github.com>
2023-11-15 16:53:37 -0600

    First try

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page_test);
+
+        CardView cardView1 = findViewById(R.id.Card1);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 启动新的 Activity
+                Intent intent = new Intent(MainActivity.this, MarketDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView cardView2 = findViewById(R.id.Card2);
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 启动新的 Activity
+                Intent intent = new Intent(MainActivity.this, MarketDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 为其他 CardViews 设置监听器...
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MarketDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MarketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 引用您的布局文件
+        setContentView(R.layout.activity_marketdetail);
+    }
+}
+
