0e921ece574dcffb4b75a76e91fb45032f77cae1
Niyatee Desai <niyateedesai@wirelessprv-10-192-210-92.near.illinois.edu>
2023-11-16 16:53:51 -0600

    add new event stuff

--- app/src/main/java/com/example/myapplication/VendorLogIn.java
+++ app/src/main/java/com/example/myapplication/VendorLogIn.java
@@ -7,25 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-public class MarketDetailActivity extends AppCompatActivity {
+public class VendorLogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 引用您的布局文件
-        setContentView(R.layout.activity_marketdetail);
+        setContentView(R.layout.vendor_login);
 
-        Button new_market_button = findViewById(R.id.addNewMarketButton);
+        Button log_in_button = findViewById(R.id.log_in_button);
 
-        new_market_button.setOnClickListener(new View.OnClickListener() {
+        log_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MarketDetailActivity.this, VendorLogIn.class);
+                Intent intent = new Intent(VendorLogIn.this, AddEvent.class);
                 startActivity(intent);
             }
         });
+
     }
 }
 
