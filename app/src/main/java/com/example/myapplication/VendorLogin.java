71f2991c489dd1fb8e0a57aea0affb564fda8e7b
PasserBo <PasserBo>
2023-11-30 20:32:11 -0600

    Update VendorLogin.java

--- app/src/main/java/com/example/myapplication/VendorLogin.java
+++ app/src/main/java/com/example/myapplication/VendorLogin.java
@@ -1,30 +1,72 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-public class VendorLogin extends AppCompatActivity {
+public class VendorLogIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 引用您的布局文件
         setContentView(R.layout.vendor_login);
 
-        Button log_in_button = findViewById(R.id.log_in_button);
-
-        log_in_button.setOnClickListener(new View.OnClickListener() {
+        Button home_button= findViewById(R.id.home_btn);
+        home_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(VendorLogin.this, AddNewEvent.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Intent intentHome = new Intent(VendorLogIn.this, MainActivity.class);
+                intentHome.putExtra("Activity", "VendorLogIn");
+                startActivity(intentHome);
             }
         });
-
+        Button profile_button = (Button) findViewById(R.id.profileBtn);
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentProfile = new Intent(VendorLogIn.this, VendorLogIn.class);
+                intentProfile.putExtra("Activity", "VendorLogIn");
+                startActivity(intentProfile);
+            }
+        });
+        Button back_button = (Button) findViewById(R.id.backBtn);
+        String previousActivity = getIntent().getStringExtra("Activity");
+        if (previousActivity.equals("MarketDetailActivity")) {
+            back_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent back = new Intent(VendorLogIn.this, MarketDetailActivity.class);
+                    startActivity(back);
+                }
+            });
+        } else if (previousActivity.equals("VendorLogIn")) {
+            back_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent back = new Intent(VendorLogIn.this, VendorLogIn.class);
+                    startActivity(back);
+                }
+            });
+        } else if (previousActivity.equals("VendorOne")) {
+            back_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent back = new Intent(VendorLogIn.this, VendorOne.class);
+                    startActivity(back);
+                }
+            });
+        } else {
+            back_button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent back = new Intent(VendorLogIn.this, MainActivity.class);
+                    startActivity(back);
+                }
+            });
+        }
     }
 }
